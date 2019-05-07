36f024745c257da6ac46cf67c9446aeb2bfb4699
dgarsir <welch.carlton@gmail.com>
2019-05-06 23:17:46 -0400

    Updates

---
+++ precache-manifest.52614c601639fd188b2f91085c7e7805.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "587e2a3e7d8126182b9b",
+    "url": "/PooledHouse/static/css/main.9d1aa87d.chunk.css"
+  },
+  {
+    "revision": "587e2a3e7d8126182b9b",
+    "url": "/PooledHouse/static/js/main.bb8c0d04.chunk.js"
+  },
+  {
+    "revision": "d44023f6eae79a485d2c",
+    "url": "/PooledHouse/static/js/runtime~main.6304562f.js"
+  },
+  {
+    "revision": "bba9b940ec963ab4f956",
+    "url": "/PooledHouse/static/css/2.e6f20e3a.chunk.css"
+  },
+  {
+    "revision": "bba9b940ec963ab4f956",
+    "url": "/PooledHouse/static/js/2.f79df2c3.chunk.js"
+  },
+  {
+    "revision": "8e0f60ef3a4b6b1ef3f8b54448812f76",
+    "url": "/PooledHouse/static/media/logo.8e0f60ef.png"
+  },
+  {
+    "revision": "afebe02ee4e52f97872a32436efa87b4",
+    "url": "/PooledHouse/index.html"
+  }
+];
